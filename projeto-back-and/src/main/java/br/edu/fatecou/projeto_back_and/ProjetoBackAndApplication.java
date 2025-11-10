113c69d20de04c4fac6a7da50f1c0a8e9eaae04e
ottoms <ottom.simoes@gmail.com>
2025-11-10 15:26:16 +0000

    Criação do projeto

---
+++ projeto-back-and/src/main/java/br/edu/fatecou/projeto_back_and/ProjetoBackAndApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecou.projeto_back_and;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackAndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackAndApplication.class, args);
+	}
+
+}
---
+++ projeto-back-and/src/test/java/br/edu/fatecou/projeto_back_and/ProjetoBackAndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecou.projeto_back_and;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackAndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
